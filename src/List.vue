e02b3a57f2d685f5df36c0d77cf385b3a4b68e9b
Simon Zhang <muyezhang@gmail.com>
2019-07-17 03:41:51 +0800

    Initial commit

---
+++ src/List.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Filters & Mixins</h1>
+        <input v-model="filterText">
+        <ul>
+            <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import { fruitMixin } from './fruitMixin';
+
+    export default {
+        data () {
+          return {
+            fruitMixin: fruitMixin
+          }
+        },
+        created() {
+            console.log('Inside List Created Hook');
+        }
+    }
+</script>
+
+<style>
+</style>
